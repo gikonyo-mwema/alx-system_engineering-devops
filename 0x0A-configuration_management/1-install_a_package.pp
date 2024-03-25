923d3f748e875e3d5f8135f3b5d64673135f44a8
gikonyo-mwema <gikonyo.mwema@gmail.com>
2024-03-24 19:04:08 -0700

    flask install

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
-# Installs puppet-lint
-exec { 'install puppet-lint':
-  path    => '/usr/local/bin/puppet-lint',
-  command => 'gem install puppet-lint -v 2.1.1',
+#!/usr/bin/pup
+# Install an especific version of flask (2.1.0)
+package {'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
