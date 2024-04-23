df938b5bc2fb4d7202c3fffb0f4e30a2c81119a1
gikonyo-mwema <gikonyo.mwema@gmail.com>
2024-04-23 04:51:56 +0300

    main moved up

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -10,31 +10,28 @@ import requests
 import sys
 
 
-def get_employee_todo_list(employee_id):
-    """
-    Fetches and exports the TODO list progress for a given employee ID.
-    """
-    base_url = "https://jsonplaceholder.typicode.com/"
-    user_url = "{}users/{}".format(base_url, employee_id)
-    todos_url = "{}todos?userId={}".format(base_url, employee_id)
-
-    user = requests.get(user_url).json()
-    todos = requests.get(todos_url).json()
-
-    with open('{}.csv'.format(employee_id), 'w', newline='') as file:
-        writer = csv.writer(file, quoting=csv.QUOTE_ALL)
-        writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS",
-                         "TASK_TITLE"])
-
-        for task in todos:
-            writer.writerow([employee_id, user.get('username'),
-                             task.get('completed'), task.get('title')])
-
-
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         print('Usage: {} <employee ID>'.format(sys.argv[0]))
         sys.exit(1)
 
     employee_id = sys.argv[1]
-    get_employee_todo_list(employee_id)
+    base_url = "https://jsonplaceholder.typicode.com/"
+    users_url = "{}users".format(base_url)
+    todos_url = "{}todos".format(base_url)
+
+    users = requests.get(users_url).json()
+    todos = requests.get(todos_url).json()
+
+    username = [user['username'] for user in users
+                if user['id'] == int(employee_id)][0]
+
+    with open('{}.csv'.format(employee_id), 'w', newline='') as file:
+        writer = csv.writer(file, quoting=csv.QUOTE_ALL)
+        writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS",
+                         "TASK_TITLE"])
+
+        for todo in todos:
+            if todo['userId'] == int(employee_id):
+                writer.writerow([employee_id, username,
+                                 todo['completed'], todo['title']])
