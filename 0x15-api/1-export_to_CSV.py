f1b457410a75d041ff641e27de8f423148f7aa16
gikonyo-mwema <gikonyo.mwema@gmail.com>
2024-04-23 04:37:40 +0300

    improvement

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -14,21 +14,25 @@ def get_employee_todo_list(employee_id):
     """
     Fetches and exports the TODO list progress for a given employee ID.
     """
-    base_url = "https://jsonplaceholder.typicode.com/users/"
-    user_url = "{}{}".format(base_url, employee_id)
-    todos_url = "{}todos?userId={}".format(base_url, employee_id)
+    base_url = "https://jsonplaceholder.typicode.com/"
+    users_url = "{}users".format(base_url)
+    todos_url = "{}todos".format(base_url)
 
-    user = requests.get(user_url).json()
+    users = requests.get(users_url).json()
     todos = requests.get(todos_url).json()
+
+    username = [user['username'] for user in users
+                if user['id'] == int(employee_id)][0]
 
     with open('{}.csv'.format(employee_id), 'w', newline='') as file:
         writer = csv.writer(file, quoting=csv.QUOTE_ALL)
-        writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS",
-                         "TASK_TITLE"])
+        writer.writerow(["USER_ID", "USERNAME",
+                         "TASK_COMPLETED_STATUS", "TASK_TITLE"])
 
-        for task in todos:
-            writer.writerow([employee_id, user.get('username'),
-                             task.get('completed'), task.get('title')])
+        for todo in todos:
+            if todo['userId'] == int(employee_id):
+                writer.writerow([employee_id, username,
+                                 todo['completed'], todo['title']])
 
 
 if __name__ == "__main__":
