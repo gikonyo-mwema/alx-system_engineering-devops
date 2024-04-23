38ba25035354207ebdfcbda8896d5ff3e9099b77
gikonyo-mwema <gikonyo.mwema@gmail.com>
2024-04-23 05:01:51 +0300

    builder

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -5,7 +5,6 @@ It accepts an employee ID as a command-line argument and exports the
 employee's TODO list progress in a specific CSV format.
 """
 
-import csv
 import requests
 import sys
 
@@ -15,23 +14,23 @@ if __name__ == "__main__":
         print('Usage: {} <employee ID>'.format(sys.argv[0]))
         sys.exit(1)
 
-    employee_id = sys.argv[1]
+    employee_id = int(sys.argv[1])
     base_url = "https://jsonplaceholder.typicode.com/"
-    users_url = "{}users".format(base_url)
+    user_url = "{}users/{}".format(base_url, employee_id)
     todos_url = "{}todos".format(base_url)
 
-    users = requests.get(users_url).json()
+    users = requests.get(user_url).json()
     todos = requests.get(todos_url).json()
 
     username = [user['username'] for user in users
-                if user['id'] == int(employee_id)][0]
+                if user['id'] == employee_id][0]
 
-    with open('{}.csv'.format(employee_id), 'w', newline='') as file:
-        writer = csv.writer(file, quoting=csv.QUOTE_ALL)
-        writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS",
-                         "TASK_TITLE"])
+    # build the csv
+    builder = ""
+    for todo in todos:
+        if todo['userId'] == employee_id:
+            builder += '"{}","{}","{}","{}"\n'.format(
+                employee_id, username, todo['completed'], todo['title'])
 
-        for todo in todos:
-            if todo['userId'] == int(employee_id):
-                writer.writerow([employee_id, username,
-                                 todo['completed'], todo['title']])
+    with open('{}.csv'.format(employee_id), 'w', encoding='UTF8') as myFile:
+        myFile.write(builder)
