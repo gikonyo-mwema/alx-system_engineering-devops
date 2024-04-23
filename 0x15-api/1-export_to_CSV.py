009088c958a27cb6a19e8d66687d2487500676d5
gikonyo-mwema <gikonyo.mwema@gmail.com>
2024-04-23 04:16:23 +0300

    csv

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -2,7 +2,7 @@
 """
 This module uses requests to interact with the JSONPlaceholder API.
 It accepts an employee ID as a command-line argument and exports the
-employee's TODO list progress in a CSV file.
+employee's TODO list progress in a specific CSV format.
 """
 
 import csv
@@ -10,7 +10,7 @@ import requests
 import sys
 
 
-def export_employee_todo_list(employee_id):
+def get_employee_todo_list(employee_id):
     """
     Fetches and exports the TODO list progress for a given employee ID.
     """
@@ -22,7 +22,10 @@ def export_employee_todo_list(employee_id):
     todos = requests.get(todos_url).json()
 
     with open('{}.csv'.format(employee_id), 'w', newline='') as file:
-        writer = csv.writer(file, quoting=csv.QUOTE_ALL)
+        writer = csv.writer(file)
+        writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS",
+                         "TASK_TITLE"])
+
         for task in todos:
             writer.writerow([employee_id, user.get('name'),
                              task.get('completed'), task.get('title')])
@@ -34,4 +37,4 @@ if __name__ == "__main__":
         sys.exit(1)
 
     employee_id = sys.argv[1]
-    export_employee_todo_list(employee_id)
+    get_employee_todo_list(employee_id)
