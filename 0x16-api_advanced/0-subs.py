8724d17beb1f08cb4c32efa79bd63b11ea750267
gikonyo-mwema <gikonyo.mwema@gmail.com>
2024-05-08 05:41:23 +0300

    comments added

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,18 +4,15 @@ Subscribers
 """
 import requests
 
+
 def number_of_subscribers(subreddit):
-    headers = {'User-Agent': 'My User Agent 1.0'}
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code != 200:
+    """
+    Function that queries the Reddit API
+    """
+    url = requests.get(
+            'https://www.reddit.com/r/{}/about.json'.format(subreddit),
+            headers={"User-Agent": "Custom"},
+    )
+    if url.status_code == 200:
         return 0
     return response.json().get('data').get('subscribers')
-
-
-if __name__ == '__main__':
-    import sys
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-    else:
-        print("{:d}".format(number_of_subscribers(sys.argv[1])))
