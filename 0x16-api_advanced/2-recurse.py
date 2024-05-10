404abb2d3d1f72bed65ba3cf98ca09c63b571bf9
gikonyo-mwema <gikonyo.mwema@gmail.com>
2024-05-10 04:05:26 +0300

    new user name created and added

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -2,33 +2,31 @@
 """
 Recursive function
 """
+
 import requests
 
 
-def recurse(subreddit, hot_list=[], after=""):
-    headers = {'User-Agent': 'My User Agent 1.0'}
-    base_url = 'https://www.reddit.com/r/{}/hot.json?after={}'
-    url = base_url.format(subreddit, after)
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code != 200:
-        print(None)
-    else:
-        data = response.json().get('data')
-        for post in data.get('children'):
-            hot_list.append(post.get('data').get('title'))
-        after = data.get('after')
-        if after is None:
+def recurse(subreddit, hot_list=[], after=None):
+    """function that queries the Reddit API and prints the titles of
+    the first 10 hot posts listed for a given subreddit."""
+
+    headers = {'User-Agent': 'gstudio254'}
+    params = {"limit": 100, 'after': after}
+    response = requests.get("https://www.reddit.com/r/{}/hot/.json".
+                            format(subreddit), headers=headers, params=params)
+
+    if response:
+        after = response.json().get("data").get("after")
+        if after:
+            recurse(subreddit, hot_list, after=after)
+            titles = response.json().get("data").get("children")
+            for title in titles:
+                hot_list.append(title.get('data').get('title'))
             return hot_list
-        return recurse(subreddit, hot_list, after)
-
-
-if __name__ == '__main__':
-    import sys
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-    else:
-        result = recurse(sys.argv[1])
-        if result is not None:
-            print(len(result))
         else:
-            print("None")
+            titles = response.json().get("data").get("children")
+            for title in titles:
+                hot_list.append(title.get("data").get("title"))
+            return hot_list
+    else:
+        return None
