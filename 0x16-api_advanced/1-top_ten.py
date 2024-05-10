cb1fc42025444a386a67b563e4e9cb41125c5106
gikonyo-mwema <gikonyo.mwema@gmail.com>
2024-05-10 04:21:46 +0300

    after and limit

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,24 +1,22 @@
 #!/usr/bin/python3
 """
-Function that queries the Reddit API
+Queries the Reddit API.
 """
-
 import requests
 
 
 def top_ten(subreddit):
     """
-    function that queries the Reddit API and prints the titles of
-    the first 10 hot posts listed for a given subreddit.
+    Queries the Reddit API and prints the titles of the first 10 hot posts
+    listed for a given subreddit.
     """
-
-    headers = {'User-Agent': 'gstudio254'}
-    response = requests.get("https://www.reddit.com/r/{}/hot/.json".
-                            format(subreddit), headers=headers)
-
-    if response:
-        children = response.json().get("data").get("children")
-        for i in range(10):
-            print(children[i].get("data").get("title"))
-    else:
-        print(None)
+    url = 'https://www.reddit.com/r/{}/hot.json?show="all"&limit=10'.format(
+        subreddit)
+    headers = {'User-Agent': 'Python/1.0(Holberton School 0x16)'}
+    response = requests.get(url, headers=headers)
+    try:
+        top_ten = response.json()['data']['children']
+        for post in top_ten:
+            print(post['data']['title'])
+    except KeyError:
+        print("None")
